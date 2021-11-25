39afa83caa7128b9ad3772909202708bd4082b01
Adam Cox <8037782+adamjhc@users.noreply.github.com>
2021-11-25 15:59:59 +0000

    Complete Day 01 Part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -2,15 +2,38 @@ use utils;
 
 fn main() {
     println!("{}", part_1());
+    println!("{}", part_2());
 }
 
-fn part_1() -> usize {
+fn part_1() -> isize {
     utils::read_lines("./input.txt")
-        .map(|a| calculate_required_fuel(a.unwrap().as_str().parse().unwrap()))
-        .sum::<usize>()
+        .map(|mass_input| calculate_required_fuel(mass_input.unwrap().as_str().parse().unwrap()))
+        .sum()
 }
 
-fn calculate_required_fuel(mass: usize) -> usize {
+fn part_2() -> isize {
+    utils::read_lines("./input.txt")
+        .map(|mass_input| {
+            let mut fuel: isize = 0;
+            let mass: isize = mass_input.unwrap().as_str().parse().unwrap();
+
+            let mut extra_fuel: isize = mass as isize;
+            loop {
+                extra_fuel = calculate_required_fuel(extra_fuel);
+
+                if extra_fuel <= 0 {
+                    break;
+                }
+
+                fuel += extra_fuel;
+            }
+
+            fuel
+        })
+        .sum()
+}
+
+fn calculate_required_fuel(mass: isize) -> isize {
     mass / 3 - 2
 }
 
@@ -21,5 +44,10 @@ mod day_01_tests {
     #[test]
     fn part_1_gives_correct_answer() {
         assert_eq!(part_1(), 3216744);
+    }
+
+    #[test]
+    fn part_2_gives_correct_answer() {
+        assert_eq!(part_2(), 4822249);
     }
 }
