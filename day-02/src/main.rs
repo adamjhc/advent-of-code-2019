232e50a9b1d9e05fc3bafe9709e15c093b0488da
Adam Cox <8037782+adamjhc@users.noreply.github.com>
2021-11-26 09:51:21 +0000

    Complete Day 02 Part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -2,27 +2,62 @@ use utils;
 
 fn main() {
     println!("{}", part_1());
+    println!("{}", part_2());
 }
 
 fn part_1() -> usize {
-    let mut intcode: Vec<usize> = utils::read_string("./input.txt")
+    let mut memory = read_memory();
+
+    memory[1] = 12;
+    memory[2] = 2;
+
+    memory = run_intcode(memory);
+
+    memory[0]
+}
+
+fn part_2() -> usize {
+    let memory_base = read_memory();
+
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            let mut memory = memory_base.clone();
+
+            memory[1] = noun;
+            memory[2] = verb;
+
+            memory = run_intcode(memory);
+
+            if memory[0] == 19690720 {
+                return 100 * noun + verb;
+            }
+        }
+    }
+
+    panic!("No answer found :/")
+}
+
+fn read_memory() -> Vec<usize> {
+    utils::read_string("./input.txt")
         .split(',')
         .map(|a| a.parse().unwrap())
-        .collect();
+        .collect()
+}
 
-    intcode[1] = 12;
-    intcode[2] = 2;
-
-    let mut pc = 0;
+fn run_intcode(mut memory: Vec<usize>) -> Vec<usize> {
+    let mut instruction_pointer = 0;
     loop {
-        match intcode[pc] {
+        // match opcode
+        match memory[instruction_pointer] {
             1 => {
-                let output_addr = intcode[pc + 3];
-                intcode[output_addr] = intcode[intcode[pc + 1]] + intcode[intcode[pc + 2]]
+                let output_addr = memory[instruction_pointer + 3];
+                memory[output_addr] = memory[memory[instruction_pointer + 1]]
+                    + memory[memory[instruction_pointer + 2]]
             }
             2 => {
-                let output_addr = intcode[pc + 3];
-                intcode[output_addr] = intcode[intcode[pc + 1]] * intcode[intcode[pc + 2]]
+                let output_addr = memory[instruction_pointer + 3];
+                memory[output_addr] = memory[memory[instruction_pointer + 1]]
+                    * memory[memory[instruction_pointer + 2]]
             }
             99 => {
                 break;
@@ -30,10 +65,10 @@ fn part_1() -> usize {
             _ => {}
         }
 
-        pc += 4;
+        instruction_pointer += 4;
     }
 
-    intcode[0]
+    memory
 }
 
 #[cfg(test)]
@@ -43,5 +78,10 @@ mod day_01_tests {
     #[test]
     fn part_1_gives_correct_answer() {
         assert_eq!(part_1(), 5866663);
+    }
+
+    #[test]
+    fn part_2_gives_correct_answer() {
+        assert_eq!(part_2(), 4259);
     }
 }
