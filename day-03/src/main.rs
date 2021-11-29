fd8ba306e6da9409e3fb9801320289ecc569e47e
Adam Cox <8037782+adamjhc@users.noreply.github.com>
2021-11-29 15:32:20 +0000

    Remove commented solution 2 code

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -17,12 +17,6 @@ struct Pos {
     y: isize,
 }
 
-// #[derive(PartialEq)]
-// enum Orientation {
-//     Horizontal,
-//     Vertical,
-// }
-
 fn part_1() -> isize {
     let paths: Vec<Vec<String>> = utils::read_string("./input.txt")
         .split("\n")
@@ -33,34 +27,6 @@ fn part_1() -> isize {
     let path_2 = generate_path(&paths[1]);
 
     let intersections: Vec<&Pos> = path_1.iter().filter(|&pos| path_2.contains(pos)).collect();
-
-    // for i in 0..(path_1.len() - 1) {
-    //     let line_1 = (path_1[i], path_1[i + 1]);
-    //     let line_1_orientation = get_orientation(line_1);
-
-    //     for j in 0..(path_2.len() - 1) {
-    //         let line_2 = (path_2[j], path_2[j + 1]);
-    //         let line_2_orientation = get_orientation(line_2);
-
-    //         if line_1_orientation == line_2_orientation {
-    //             match line_1_orientation {
-    //                 Orientation::Vertical => {
-    //                     if line_1.0.x == line_2.0.x {
-    //                         if line_1.0.y < line_1.1.y {
-    //                             if line_2.0.y.is_between(line_1.0.y, line_1.1.y) {
-    //                             } else if line_2.1.y.is_between(line_1.0.y, line_1.1.y) {
-    //                             }
-    //                         } else {
-    //                             return ((b.y1 > a.y2 && b.y1 < a.y1)
-    //                                 || (b.y2 > a.y2 && b.y2 < a.y1));
-    //                         }
-    //                     }
-    //                 }
-    //                 Orientation::Horizontal => if line_1.0.y == line_2.0.y {},
-    //             }
-    //         }
-    //     }
-    // }
 
     intersections
         .iter()
@@ -98,41 +64,6 @@ fn generate_path(instructions: &Vec<String>) -> Vec<Pos> {
 
     path
 }
-
-// fn generate_path(instructions: &Vec<String>) -> Vec<Pos> {
-//     let mut path: Vec<Pos> = Vec::new();
-//     let mut x = 0;
-//     let mut y = 0;
-//     path.push(Pos { x, y });
-//     for instruction in instructions {
-//         let distance: usize = instruction
-//             .chars()
-//             .skip(1)
-//             .collect::<String>()
-//             .parse()
-//             .unwrap();
-
-//         match instruction.as_bytes()[0] as char {
-//             'R' => x += distance,
-//             'U' => y += distance,
-//             'L' => x -= distance,
-//             'D' => y -= distance,
-//             _ => panic!("Unexpected "),
-//         }
-
-//         path.push(Pos { x, y });
-//     }
-
-//     path
-// }
-
-// fn get_orientation(line: (Pos, Pos)) -> Orientation {
-//     if line.0.x == line.1.x {
-//         Orientation::Vertical
-//     } else {
-//         Orientation::Horizontal
-//     }
-// }
 
 fn calculate_manhatten_distance(pos: &Pos) -> isize {
     pos.x.abs() + pos.y.abs()
