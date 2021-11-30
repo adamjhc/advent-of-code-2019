696e25020302a72e2b02fc6d5f5afe0e84cd63b5
Adam Cox <8037782+adamjhc@users.noreply.github.com>
2021-11-30 16:03:53 +0000

    Complete Day 04 Part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -60,30 +60,18 @@ fn digits_never_decrease(digits: &[u8]) -> bool {
 fn has_separate_adjacent_digits(digits: &[u8]) -> bool {
     let mut has_same_adjacent_digits = false;
     let mut lock = false;
-    // let mut last_same_digit_pos = 0usize;
-    // let mut last_same_digit = 0u8;
     for n in 1..digits.len() {
         if digits[n - 1] == digits[n] {
-            if has_same_adjacent_digits {
-                if !lock {
-                    has_same_adjacent_digits = false;
-                    lock = true;
-                }
-            } else {
-                if !lock {
-                    has_same_adjacent_digits = true;
-                }
+            if has_same_adjacent_digits && !lock {
+                has_same_adjacent_digits = false;
+                lock = true;
+            } else if !lock {
+                has_same_adjacent_digits = true;
             }
-
-            // if has_same_adjacent_digits {
-            //     if last_same_digit_pos == n - 1 && last_same_digit == digits[n] {
-            //         has_same_adjacent_digits = false;
-            //         last_same_digit_pos = n;
-            //     }
-            // } else {
-            //     has_same_adjacent_digits = true;
-            // }
         } else {
+            if has_same_adjacent_digits {
+                return true;
+            }
             lock = false;
         }
     }
@@ -98,6 +86,11 @@ mod day_04_tests {
     #[test]
     fn part_1_gives_correct_answer() {
         assert_eq!(part_1(), 1079)
+    }
+
+    #[test]
+    fn part_2_gives_correct_answer() {
+        assert_eq!(part_2(), 699)
     }
 
     #[test]
